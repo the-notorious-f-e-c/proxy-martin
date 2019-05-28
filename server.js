7582e01a6f73ac8217eb53e867f93fe5e7a5c7ec
Martin Connor <martinconnor@Martins-MacBook-Pro-3.local>
2019-05-28 13:45:18 -0700

    Initial commit.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app  = express();
+
+// Eugenia's module: (3000)
+
+// Rafe's module: (3002)
+
+// Erik's module: (3003)
+// app.get()
+// req.redirect 
+
+
+// My module: (3000)
+
+// app.listen()
